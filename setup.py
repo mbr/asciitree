04eba6def111b42b1eb19345e0fc18e5ac897f4a
Marc Brinkmann <git@marcbrinkmann.de>
2015-02-26 23:54:35 +0100

    A more modern structure for the package.

--- setup.py
+++ setup.py
@@ -12,12 +12,13 @@ def read(fname):
 
 setup(
     name='asciitree',
-    version='0.2',
+    version='0.3dev',
     description='Draws ASCII trees.',
     long_description=read('README.rst'),
     author='Marc Brinkmann',
     author_email='git@marcbrinkmann.de',
     url='http://github.com/mbr/asciitree',
     license='MIT',
-    py_modules=['asciitree'],
+    packages=find_packages(exclude=['tests']),
+    install_requires=[],
 )
