620a2ecaa0b25a1f57e4424ae77d8fd71babb5f3
Marc Brinkmann <git@marcbrinkmann.de>
2015-02-27 11:07:19 +0100

    Made example a little more complex.

--- example.py
+++ example.py
@@ -1,7 +1,11 @@
 from asciitree import ascii_tree
 from collections import OrderedDict as OD
+from copy import deepcopy
 
 
+# a basic tree
+# OrderedDict is used in some places where node order is important, otherwise
+# a normal dict is used for the sake of readabilitiy
 tree = {
     'asciitree': OD([
         ('sometimes',
@@ -21,3 +25,11 @@ tree = {
 }
 
 print ascii_tree(tree)
+print
+
+
+# construct a more complex tree by copying the tree and grafting it onto itself
+tree2 = deepcopy(tree)
+tree2['asciitree']['trees'] = deepcopy(tree2['asciitree'])
+
+print ascii_tree(tree2)
