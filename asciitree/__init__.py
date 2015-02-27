02668b8dfda3c00f4ae74846d7c14c5dde64e17c
Marc Brinkmann <git@marcbrinkmann.de>
2015-02-27 11:32:54 +0100

    Remove now obsolete ascii_tree function.

--- asciitree/__init__.py
+++ asciitree/__init__.py
@@ -4,13 +4,6 @@
 from legacy import draw_tree, _draw_tree
 
 
-def ascii_tree(node,
-               get_node_children=lambda t: t[1].items(),
-               get_node_text=lambda t: t[0],
-               get_root=lambda d: d.items()[0]):
-    return _draw_tree(get_root(node), '', get_node_children, get_node_text)
-
-
 def left_aligned(tree,
                  get_node_children=lambda t: t[1].items(),
                  get_node_text=lambda t: t[0],
