8f419f5c6a0c234190dfbcf36d7db0cc86c1e585
Marc Brinkmann <git@marcbrinkmann.de>
2016-09-05 21:05:29 +0200

    Port example to Python3 (closes #8).

--- example.py
+++ example.py
@@ -6,7 +6,6 @@ from asciitree.drawing import BoxStyle, BOX_DOUBLE, BOX_BLANK
 
 tr = LeftAligned()
 
-
 # a basic tree
 # OrderedDict is used in some places where node order is important, otherwise
 # a normal dict is used for the sake of readabilitiy
@@ -28,23 +27,20 @@ tree = {
     ])
 }
 
-print tr(tree)
+print(tr(tree))
 
 # construct a more complex tree by copying the tree and grafting it onto itself
 tree2 = deepcopy(tree)
 tree2['asciitree']['trees'] = deepcopy(tree2['asciitree'])
-print tr(tree2)
-
+print(tr(tree2))
 
 # use a box style
 box_tr = LeftAligned(draw=BoxStyle(gfx=BOX_DOUBLE, horiz_len=1))
-print box_tr(tree)
-
+print(box_tr(tree))
 
 # more airy
 air_tr = LeftAligned(draw=BoxStyle(gfx=BOX_BLANK,
                                    label_space=0,
                                    label_format='[{}]',
-                                   indent=0)
-                     )
-print air_tr(tree)
+                                   indent=0))
+print(air_tr(tree))
